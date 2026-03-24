a1d9646e4ad38549819044bc7004e411b22fac88
Ryan <58894418+Tnbryan@users.noreply.github.com>
2026-03-24 15:05:10 +0000

    Atualizando main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "task-manager.h"
 
+#include <stdio.h>
+
+    struct Tasks
+    {
+        int id;
+        char nome;
+        int prioridade;
+    };
+    
+
+
 int main() {
   return 0;
 }
