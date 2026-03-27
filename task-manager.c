a021b01854704ab8fd753d719593c760e1b808b4
Ryan <58894418+Tnbryan@users.noreply.github.com>
2026-03-27 13:49:02 +0000

    malloc adicionado porem

--- task-manager.c
+++ task-manager.c
@@ -13,8 +13,8 @@ Task* create_task_coleta(){
 
     printf("ID da ATIVIDADE : ");
     scanf("%d", &id);
-    
-    getchar(); // limpa o buffer
+
+    getchar(); // limpa os espaços vazios
 
     printf("Qual a Atividade ? : ");
     fgets(nameTask, 100, stdin);
@@ -23,4 +23,27 @@ Task* create_task_coleta(){
     scanf("%d", &priority);
 
     return create_task(id,nameTask,priority);
+}
+
+// é necessario criar uma função MALLOC para armazenar os dados pois uma função normal não guarda os dados !
+
+Task* create_task(int id, const char* nameTask, enum TaskPriority priority){
+    Task *ponteiro = malloc(sizeof(Task));
+      if (!ponteiro) return NULL; // sempre em um MAllOC, deve verificar se funcionou
+
+    ponteiro->id=id;
+    ponteiro->is_completed = 0;
+    ponteiro->priority = priority;
+
+// par armazenar um texto é bem complicado e precisa utilizar o STRCPY mas antes precisa alocar ele primeiro
+                    // essa função aloca ele e após isso utiliza-se o STRCPY.  
+
+    ponteiro->nameTask = malloc(strlen(nameTask) + 1); // é uma verificação
+    if (!ponteiro->nameTask){
+        free(ponteiro);        
+        return NULL;
+    }
+    strcpy(ponteiro->nameTask, nameTask);
+
+    return ponteiro;
 }
